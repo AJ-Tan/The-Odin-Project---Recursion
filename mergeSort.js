ca231920c826b50318cb89aa17087ac4ed0d5618
AJ-Tan <142216121+AJ-Tan@users.noreply.github.com>
2025-12-18 15:38:37 +0800

    Update sort with efficient number of iterations.

--- mergeSort.js
+++ mergeSort.js
@@ -1,16 +1,29 @@
 const arr = [3, 2, 1, 13, 8, 5, 1, 0];
+let countIteration = 0;
 
-const mergeSort = (arr, i = 0) => {
-  if (i >= arr.length - 1) {
-    return arr;
-  }
-  if (arr[i] > arr[i + 1]) {
-    [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
-    arr = mergeSort(arr, 0);
+const mergeSort = (arr, result = []) => {
+  if(arr.length <= 0) return result;
+
+  const currentNum = arr.shift();
+
+  if(result.length <= 0) {
+    result.push(currentNum);
   } else {
-    arr = mergeSort(arr, i + 1);
+    for(let i = 0; i < result.length; i++) {
+      countIteration += 1;
+      if(currentNum < result[i]) {
+        result.splice(i, 0, currentNum);
+        break;
+      }
+      if(i === result.length - 1) {
+        result.push(currentNum);
+        break;
+      }
+    }
   }
-  return arr;
-};
 
-console.log(mergeSort(arr));
+  return mergeSort(arr, result);
+}
+
+console.log(mergeSort(arr)); // [0, 1, 1, 2, 3, 5, 8, 13]
+console.log(countIteration); // 16 Iterations
