84fb7dcaf0769f308d2e7e0c89e9d9677b25bb7d
AJ-Tan <142216121+AJ-Tan@users.noreply.github.com>
2025-12-18 14:47:40 +0800

    Simplify fibonacci function implementation
    Refactor fibonacci function for conciseness and clarity.

--- fibonacci.js
+++ fibonacci.js
@@ -1,13 +1,7 @@
 const fibonachi = (counter, result = [0]) => {
-  if (counter <= 1) {
-    return result;
-  }
-  const len = result.length - 1;
-
-  result.push(
-    result[len] + (result[len - 1] !== undefined ? result[len - 1] : 1)
-  );
-  return fibonachi(counter - 1, result);
-};
+  if(counter <= 1) return result;
+  const len = result.length -1;
+  return fibonachi(counter - 1, [...result, result[len] + (result[len - 1] ?? 1)])
+}
 
 console.log(fibonachi(8));
