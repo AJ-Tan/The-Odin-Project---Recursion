c8877e604f27a21e375cc6ac2ed8e8e4c19db5ca
AJ-Tan <aldomin22@gmail.com>
2025-12-18 12:12:01 +0800

    removed variable

--- fibonacci.js
+++ fibonacci.js
@@ -3,7 +3,6 @@ const fibonachi = (counter, result = [0]) => {
     return result;
   }
   const len = result.length - 1;
-  const x = result[len - 1] !== undefined ? result[len - 1] : 1;
 
   result.push(
     result[len] + (result[len - 1] !== undefined ? result[len - 1] : 1)
